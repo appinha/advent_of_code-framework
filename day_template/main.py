c89f76425082440a9024df265c8f5c26d53f5574
appinha <amanda_pinha@hotmail.com>
2022-12-31 17:06:40 -0300

    [day_template][main] Adds type hints

--- day_template/main.py
+++ day_template/main.py
@@ -7,12 +7,12 @@ class DayPuzzleSolver():
     def __init__(self):
         self.delimiter = ""
 
-    def solve_part_1(self, raw_input):
+    def solve_part_1(self, raw_input: str):
         bla = process_input(raw_input)
 
-    def solve_part_2(self, raw_input):
+    def solve_part_2(self, raw_input: str):
         ...
 
 
-def process_input(raw_input):
+def process_input(raw_input: str):
     ...
